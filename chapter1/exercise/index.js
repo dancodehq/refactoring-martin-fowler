9e1722c304f0119b4fb5f0ebc03ed5bc7973c279
Dan Harrison <harrison92d@gmail.com>
2024-07-31 19:12:28 +0100

    Replace inline code with function call

--- chapter1/exercise/index.js
+++ chapter1/exercise/index.js
@@ -46,11 +46,7 @@ export function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format;
+    const format = formatFunction;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
 
