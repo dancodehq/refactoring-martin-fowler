7b5ed81a34fb5338ba48043cbfa95e45883dabce
Dan Harrison <harrison92d@gmail.com>
2024-07-31 18:45:14 +0100

    Extract function

--- chapter1/exercise/index.js
+++ chapter1/exercise/index.js
@@ -26,6 +26,13 @@ export function statement(invoice, plays) {
         return plays[perf.playID];
     }
 
+    function volumeCreditsFor(perf) {
+        let volumeCredits = 0;
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        return volumeCredits;
+    }
+
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
