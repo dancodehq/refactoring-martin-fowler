46066c04ca18dd6cf3abf54d54834a5f95628f3c
Dan Harrison <harrison92d@gmail.com>
2024-07-31 18:47:57 +0100

    Replace inline code with function call

--- chapter1/exercise/index.js
+++ chapter1/exercise/index.js
@@ -42,10 +42,7 @@ export function statement(invoice, plays) {
             minimumFractionDigits: 2
         }).format;
     for (let perf of invoice.performances) {
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
         result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
