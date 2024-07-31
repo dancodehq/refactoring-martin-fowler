2a2fed707deed75c0ca298fc07ee0707fef255b8
Dan Harrison <harrison92d@gmail.com>
2024-07-31 18:26:18 +0100

    Rename variable

--- chapter1/exercise/index.js
+++ chapter1/exercise/index.js
@@ -1,20 +1,20 @@
 export function statement(invoice, plays) {
 
-    function amountFor(perf, play) {
+    function amountFor(performance, play) {
         let amount = 0;
         switch (play.type) {
             case "tragedy":
                 amount = 40000;
-                if (perf.audience > 30) {
-                    amount += 1000 * (perf.audience - 30);
+                if (performance.audience > 30) {
+                    amount += 1000 * (performance.audience - 30);
                 }
                 break;
             case "comedy":
                 amount = 30000;
-                if (perf.audience > 20) {
-                    amount += 10000 + 500 * (perf.audience - 20);
+                if (performance.audience > 20) {
+                    amount += 10000 + 500 * (performance.audience - 20);
                 }
-                amount += 300 * perf.audience;
+                amount += 300 * performance.audience;
                 break;
             default:
                 throw new Error(`unknown type: ${play.type}`);
