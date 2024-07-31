78d8fd09ee484e1e885ca22c09f76fbf78d77486
Dan Harrison <harrison92d@gmail.com>
2024-07-31 20:17:40 +0100

    Move function

--- chapter1/exercise/createStatementData.js
+++ chapter1/exercise/createStatementData.js
@@ -5,28 +5,32 @@ class PerformanceCalculator {
         this.performance = performance;
         this.play = play;
     }
+
+    get amount() {
+        let amount = 0;
+        switch (this.play.type) {
+            case "tragedy":
+                amount = 40000;
+                if (this.performance.audience > 30) {
+                    amount += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                amount = 30000;
+                if (this.performance.audience > 20) {
+                    amount += 10000 + 500 * (this.performance.audience - 20);
+                }
+                amount += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${this.play.type}`);
+        }
+        return amount;
+    }
 }
 
 function amountFor(performance) {
-    let amount = 0;
-    switch (performance.play.type) {
-        case "tragedy":
-            amount = 40000;
-            if (performance.audience > 30) {
-                amount += 1000 * (performance.audience - 30);
-            }
-            break;
-        case "comedy":
-            amount = 30000;
-            if (performance.audience > 20) {
-                amount += 10000 + 500 * (performance.audience - 20);
-            }
-            amount += 300 * performance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${performance.play.type}`);
-    }
-    return amount;
+    return new PerformanceCalculator(performance, playFor(performance)).amount;
 }
 
 function playFor(perf) {
