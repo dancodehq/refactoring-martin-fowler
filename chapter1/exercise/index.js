6e9b7aa7d269400d9aadb691afe7535a2ae1ca91
Dan Harrison <harrison92d@gmail.com>
2024-07-31 19:13:10 +0100

    Inline variable

--- chapter1/exercise/index.js
+++ chapter1/exercise/index.js
@@ -46,15 +46,15 @@ export function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = formatFunction;
+
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${formatFunction(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `Amount owed is ${formatFunction(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
