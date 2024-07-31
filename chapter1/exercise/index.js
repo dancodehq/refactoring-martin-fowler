df6ecdeaaa458b51c7ebb875e2c4f237b53d0c0b
Dan Harrison <harrison92d@gmail.com>
2024-07-31 19:17:21 +0100

    Move division into formatter function

--- chapter1/exercise/index.js
+++ chapter1/exercise/index.js
@@ -40,7 +40,7 @@ export function statement(invoice, plays) {
             {
                 style: "currency", currency: "USD",
                 minimumFractionDigits: 2
-            }).format(number);
+            }).format(number / 100);
     }
 
     let totalAmount = 0;
@@ -51,10 +51,10 @@ export function statement(invoice, plays) {
         volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
