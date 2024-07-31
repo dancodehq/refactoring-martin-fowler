b3cee43675e7e117efe4d06d6e0c77247757f36c
Dan Harrison <harrison92d@gmail.com>
2024-07-31 19:27:12 +0100

    Change function declaration

--- chapter1/exercise/index.js
+++ chapter1/exercise/index.js
@@ -51,7 +51,7 @@ export function statement(invoice, plays) {
         return volumeCredits;
     }
 
-    function appleSauce() {
+    function totalAmount() {
         let totalAmount = 0;
         for (let perf of invoice.performances) {
             totalAmount += amountFor(perf);
@@ -66,7 +66,7 @@ export function statement(invoice, plays) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
 
-    result += `Amount owed is ${usd(appleSauce())}\n`;
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 }
