aa7d4bb18b9c1aec78d7aef9635996bf2f0f40ae
Dan Harrison <harrison92d@gmail.com>
2024-07-31 20:06:22 +0100

    Extract functions

---
+++ chapter1/exercise/createStatementData.js
@@ -0,0 +1,61 @@
+import plays from './plays.json' with { type: 'json' }
+
+function amountFor(performance) {
+    let amount = 0;
+    switch (performance.play.type) {
+        case "tragedy":
+            amount = 40000;
+            if (performance.audience > 30) {
+                amount += 1000 * (performance.audience - 30);
+            }
+            break;
+        case "comedy":
+            amount = 30000;
+            if (performance.audience > 20) {
+                amount += 10000 + 500 * (performance.audience - 20);
+            }
+            amount += 300 * performance.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${performance.play.type}`);
+    }
+    return amount;
+}
+
+function playFor(perf) {
+    return plays[perf.playID];
+}
+
+function volumeCreditsFor(performance) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(performance.audience - 30, 0);
+    if ("comedy" === performance.play.type) {
+        volumeCredits += Math.floor(performance.audience / 5);
+    }
+    return volumeCredits;
+}
+
+function totalVolumeCredits(data) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+}
+
+function totalAmount(data) {
+    return data.performances.reduce((total, p) => total + p.amount, 0);
+}
+
+function enrichPerformance(performance) {
+    const result = Object.assign({}, performance);
+    result.play = playFor(result);
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    return result;
+}
+
+export function createStatementData(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+}
--- chapter1/exercise/index.js
+++ chapter1/exercise/index.js
@@ -1,39 +1,6 @@
+import { createStatementData } from "./createStatementData.js";
+
 export function statement(invoice, plays) {
-
-    function amountFor(performance) {
-        let amount = 0;
-        switch (performance.play.type) {
-            case "tragedy":
-                amount = 40000;
-                if (performance.audience > 30) {
-                    amount += 1000 * (performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                amount = 30000;
-                if (performance.audience > 20) {
-                    amount += 10000 + 500 * (performance.audience - 20);
-                }
-                amount += 300 * performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${performance.play.type}`);
-        }
-        return amount;
-    }
-
-    function playFor(perf) {
-        return plays[perf.playID];
-    }
-
-    function volumeCreditsFor(performance) {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(performance.audience - 30, 0);
-        if ("comedy" === performance.play.type) {
-            volumeCredits += Math.floor(performance.audience / 5);
-        }
-        return volumeCredits;
-    }
 
     function usd(number) {
         return new Intl.NumberFormat("en-US",
@@ -41,32 +8,7 @@ export function statement(invoice, plays) {
                 style: "currency", currency: "USD",
                 minimumFractionDigits: 2
             }).format(number / 100);
-    }
-
-    function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-    }
-
-    function totalAmount(data) {
-        return data.performances.reduce((total, p) => total + p.amount, 0);
-    }
-
-    function enrichPerformance(performance) {
-        const result = Object.assign({}, performance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
-    function createStatementData(invoice) {
-        const statementData = {};
-        statementData.customer = invoice.customer;
-        statementData.performances = invoice.performances.map(enrichPerformance);
-        statementData.totalAmount = totalAmount(statementData);
-        statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-        return statementData;
-    }
+    }    
 
     function renderPlainText(data) {
         let text = `Statement for ${data.customer}\n`;
